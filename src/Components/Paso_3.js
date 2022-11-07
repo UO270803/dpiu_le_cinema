81696b1549519ee874804cff42b1de9f841c800a
Marcos Fernandez <UO270803@uniovi.es>
2022-11-07 16:46:47 +0100

    color erroneo

--- src/Components/Paso_3.js
+++ src/Components/Paso_3.js
@@ -60,7 +60,7 @@ class Paso_3 extends React.Component {
         });
         return (
             <div>
-                <Card bordered={false} className='myCard' style={{color: "white" }}>
+                <Card bordered={false} className='myCard' style={{ background: '#1890ff', color: "white" }}>
                     <Form name="basic" labelCol={{ span: 24 / 3 }} wrapperCol={{ span: 24 / 3 }}
                         initialValues={{ remember: true, }}
                         onFinish={values => this.sendLogin(values)} autoComplete="off">
